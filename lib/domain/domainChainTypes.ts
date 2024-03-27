7f6147c53902cdff408d2c7b9912cc7eb7e5449d
Xichen Pan <xichen@zeroxtech.com>
2024-03-27 11:25:29 -0700

    feat: add Sepolia testnet

--- lib/domain/domainChainTypes.ts
+++ lib/domain/domainChainTypes.ts
@@ -1,7 +1,8 @@
 import type { DomainChainType } from 'types/api/networks';
 
 export const DOMAIN_CHAIN_TYPES: DomainChainType = {
-  '84532': { title: 'Base Testnet', icon: 'networks/base' },
+  '11155111': { title: 'ETH Sepolia', icon: 'networks/ethereum' },
+  '84532': { title: 'Base Sepolia', icon: 'networks/base' },
   '421614': { title: 'Arbitrum Sepolia', icon: 'networks/arbitrum' },
   '1': { title: 'Ethereum Mainnet', icon: 'networks/ethereum' },
 };
--- types/api/networks.ts
+++ types/api/networks.ts
@@ -1,6 +1,6 @@
 export type NetworkType = 'networks/base' | 'networks/arbitrum' | 'networks/ethereum';
-export type NetworkId = 84532 | 421614 | 1;
-export type NetworkName = 'Base Testnet' | 'Arbitrum Sepolia' | 'Ethereum Mainnet';
+export type NetworkId = 84532 | 421614 | 1 | 11155111;
+export type NetworkName = 'Base Sepolia' | 'Arbitrum Sepolia' | 'Ethereum Mainnet' | 'ETH Sepolia';
 
 export type DomainChainType = {
   [Key in NetworkId as string]: {title: NetworkName; icon: NetworkType};
