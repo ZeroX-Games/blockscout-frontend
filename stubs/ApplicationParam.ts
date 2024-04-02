8c0394f2c362c74c3711c6b0b23ab4b03f44737c
Xichen Pan <xichen@zeroxtech.com>
2024-04-02 11:14:33 -0700

    chores: domain to application

--- stubs/ApplicationParam.ts
+++ stubs/ApplicationParam.ts
@@ -2,9 +2,9 @@ import type { ApplicationParam } from 'types/api/applicationParams';
 
 export const DOMAIN_HASH = '0x2B51Ae4412F79c3c1cB12AA40Ea4ECEb4e80511a';
 
-export const DOMAIN_HASH1 = '0x2B51Ae4412F79c3c1cB12AA40Ea4ECEb4e80511b';
+export const APPLICATION_HASH1 = '0x2B51Ae4412F79c3c1cB12AA40Ea4ECEb4e80511b';
 
-export const DOMAIN_PARAMS1: ApplicationParam = {
+export const APPLICATION_PARAMS1: ApplicationParam = {
   hash: DOMAIN_HASH,
   name: 'GTA V',
   applicationID: '1',
@@ -16,7 +16,7 @@ export const DOMAIN_PARAMS1: ApplicationParam = {
   chainID: -1,
 };
 
-export const DOMAIN_PARAMS2: ApplicationParam = {
+export const APPLICATION_PARAMS2: ApplicationParam = {
   hash: DOMAIN_HASH,
   name: 'GTA V',
   applicationID: '1',
